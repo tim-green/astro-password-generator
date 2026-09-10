1176c1e5c7d1bbdc1f428f229ab83a00839f75fa
Tim <github@timgreen.ws>
2026-09-10 10:35:43 +1000

    added aiRobotsTxt & favicon config to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import aiRobotsTxt from 'astro-ai-robots-txt';
+
+import favicons from 'astro-favicons';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), aiRobotsTxt(), favicons()],
   vite: {
     plugins: [tailwindcss()]
   }
